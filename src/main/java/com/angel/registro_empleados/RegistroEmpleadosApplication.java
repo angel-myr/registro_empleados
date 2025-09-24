462c651912e05e5240caa8a803cedabbe6e4d7fe
Angel <angelmariny01@gmail.com>
2025-09-23 19:17:34 -0500

    Generación por Spring.initializr

---
+++ src/main/java/com/angel/registro_empleados/RegistroEmpleadosApplication.java
@@ -0,0 +1,13 @@
+package com.angel.registro_empleados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroEmpleadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroEmpleadosApplication.class, args);
+	}
+
+}
